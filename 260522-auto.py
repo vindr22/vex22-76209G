b78d19dd582153f4904ef7b82bb433f8a502d785
vindr22 <105640722+vindr22@users.noreply.github.com>
2022-05-26 17:05:04 +0700

    Create 260522-auto.py

---
+++ 260522-auto.py
@@ -0,0 +1,25 @@
+myVariable = 0
+counter = 0
+
+def default():
+    global myVariable, counter
+    motor_group_4.set_velocity(150, PERCENT)
+    drivetrain.set_drive_velocity(80, PERCENT)
+    drivetrain.set_turn_velocity(100, PERCENT)
+
+def when_started1():
+    global myVariable, counter
+    default()
+    motor_group_4.spin_for(REVERSE, 3100, DEGREES, wait=True)
+    drivetrain.turn_for(RIGHT, 19, DEGREES, wait=True)
+    drivetrain.drive_for(FORWARD, 1280, MM, wait=True)
+    motor_group_4.spin_for(FORWARD, 1400, DEGREES, wait=True)
+    drivetrain.turn_for(RIGHT, 100, DEGREES, wait=True)
+    drivetrain.drive_for(FORWARD, 2300, MM, wait=True)
+    motor_group_4.spin_for(REVERSE, 1400, DEGREES, wait=True)
+    drivetrain.drive_for(REVERSE, 400, MM, wait=True)
+
+# Calibrate the Drivetrain
+calibrate_drivetrain()
+
+when_started1()
